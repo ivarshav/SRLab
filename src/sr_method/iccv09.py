5f362d65af0eb4539dba0a97957326c04161c733
Unknown <inbvar@gmail.com>
2018-06-21 10:43:59 +0300

    Add `best_pick` function
    and some changes in kernel in `iccv09`

--- src/sr_method/iccv09.py
+++ src/sr_method/iccv09.py
@@ -2,11 +2,13 @@ import math
 import sys
 
 import numpy as np
+from scipy.optimize import curve_fit
 
+from sr_dataset import SRDataSet
 from sr_util import profiler
 from sr_util.kernel import Kernel
+from sr_util.sr_image_util import twoD_gaussian
 from src.sr_util import sr_image_util
-from sr_dataset import SRDataSet
 
 DEFAULT_RECONSTRUCT_LEVEL = 6
 ALPHA = 2 ** (1.0 / 3)
@@ -15,7 +17,9 @@ ALPHA = 2 ** (1.0 / 3)
 class ICCV09(object):
     def __init__(self):
         self._method_type = "iccv09"
-        self._kernel = sr_image_util.create_gaussian_kernel()
+        radius = 2
+        y, x = np.mgrid[-radius:radius + 1, -radius:radius + 1]
+        self._kernel = sr_image_util.create_asymmetric_gaussian_kernel(x, y, theta=5)
 
     def get_method_type(self):
         return self._method_type
@@ -34,14 +38,15 @@ class ICCV09(object):
         """
         # import matplotlib.pyplot as plt
 
-        sr_dataset = SRDataSet.from_sr_image(sr_image)
+        sr_dataset = SRDataSet.from_sr_image(sr_image, kernel=self._kernel)
         reconstructed_sr_image = sr_image
         construct_level = 1  # int(math.log(ratio, ALPHA) + 0.5)
         r = ALPHA
         for level in xrange(construct_level):
             reconstructed_sr_image = self._reconstruct(r, reconstructed_sr_image, sr_dataset)
-            reconstructed_sr_image = sr_image_util.back_project(reconstructed_sr_image, sr_image, 3, level + 1)
-            new_sr_dataset = SRDataSet.from_sr_image(reconstructed_sr_image)
+            reconstructed_sr_image = sr_image_util.back_project(reconstructed_sr_image, sr_image, 3, level + 1,
+                                                                kernel_params=self._kernel)
+            new_sr_dataset = SRDataSet.from_sr_image(reconstructed_sr_image, kernel=self._kernel)
             # plt.imshow(reconstructed_sr_image)
             # plt.show()
             sr_dataset.merge(new_sr_dataset)
@@ -67,10 +72,29 @@ class ICCV09(object):
         patches_without_dc, patches_dc = sr_image_util.get_patches_from(resized_sr_image, interval=4)
         high_res_patches_without_dc = sr_dataset.query(patches_without_dc)
         high_res_patches = high_res_patches_without_dc + patches_dc
+        low_res_patches = patches_without_dc + patches_dc
         high_res_data = sr_image_util.unpatchify(high_res_patches, resized_sr_image.size, self._kernel.kernel)
-        self.update_kernel(high_res_patches, patches_dc)
+        self.update_kernel(high_res_patches, low_res_patches)
         resized_sr_image.putdata(high_res_data)
         return resized_sr_image
+
+    def pick_best(self, high_res_patches, low_res_patches):
+        """
+
+        :param high_res_patches:
+        :param low_res_patches: 
+        :return:
+        """
+        RANK_SIZE = 25
+        high_patches, low_patches = [], []
+        dist_sq = np.sum((high_res_patches[:, :] - low_res_patches[:, :]) ** 2, axis=1)
+        sorted_indexes = np.argsort(dist_sq)
+        for i, index in enumerate(sorted_indexes):
+            if np.linalg.matrix_rank(high_patches) >= RANK_SIZE and np.linalg.matrix_rank(low_patches) >= RANK_SIZE:
+                break
+            high_patches.append(high_res_patches[index])
+            low_patches.append(low_res_patches[index])
+        return np.array(high_patches), np.array(low_patches)
 
     def update_kernel(self, high_res_patches, low_res_patches):
         """
